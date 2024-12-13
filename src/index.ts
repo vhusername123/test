a69b941d633fa4abbd2f85a43426d7f19d9a98c8
vhusername123 <github1@hoffvinc.de>
2024-12-13 14:03:24 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import * as fs from 'fs';
+import fs from 'fs';
+const readLineBySeparateLines = (fileName: fs.PathOrFileDescriptor) => {
+    let data = fs.readFileSync(fileName)
+    console.log('File as b<te buffer: ')
+    console.log(data)
 
-
-console.log("test");
+    console.log('File as strings:')
+    console.log(data.toString())
+    return data
+}
+const path = 'src/2024-12-09log.text';
+readLineBySeparateLines(path);
+process.stdin.on('data', function(data) {
+    let path2 = data.toString();
+    path2 = path2.replace(/(\r\n|\n|\r)/gm, "");
+    readLineBySeparateLines(path2);
+})
